b577f4e39b41b1db6c5cd0d1e01a780e48b8fb9f
Gelly Andrea N. Camungao <gellcykim@gmail.com>
2017-03-05 01:57:58 -0800

    loginpage file

--- home.php
+++ home.php
@@ -1,32 +1,16 @@
-<br><br>
 <div class="container">
-      <div class="col-xm-12 col-md-3 col-md-offset-1 ">	
-        <ul class="list-group">
-          
-          <li class="list-group-item">
-               <b><center>Jacob Cielo</center></b>
-               <center>CLIENT</center>
-          </li><br>
-        </ul>
-      </div>
-      <div class="col-md-6">	
-        <ul class="list-group">
-           <li class="list-group-item">
-              <table class='table table-hover table table-striped' border="1">
-                <thead>
-                  <tr>
-                    <th><center>Previous weight</center></th>
-                    <th><center>New Weight</center></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td><center>50 kg</center></td>
-                    <td><center>45 kg</center></td>
-                  </tr>
-                </tbody>
-              </table>
-          </li><br>
-        </ul>
-      </div>
-    </div>
+	 <div class="col-md-12 col-md-12 col-md-12">  
+	  <form id="contact" action="" method="post">
+		<h3>Please Login</h3>
+		<fieldset>
+		  <input placeholder="Username" type="text" tabindex="1" required autofocus>
+		</fieldset>
+		<fieldset>
+		  <input placeholder="Password" type="password" tabindex="1" required autofocus>
+		</fieldset>
+		<fieldset>
+		  <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">login</button>
+		</fieldset>
+	  </form>
+	</div> 
+</div>	
